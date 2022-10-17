3269392cbfe3fceef6bc65f1f0153ce4910d63b2
Vladimir Antonov <antonov.vladimir.89@gmail.com>
2022-10-17 16:53:16 +0300

    Options

--- js/script.js
+++ js/script.js
@@ -111,4 +111,27 @@ window.addEventListener("DOMContentLoaded", function () {
     more.classList.remove("more-splash");
     document.body.style.overflow = "";
   });
+
+  /* OPTIONS */
+
+  class Options {
+    constructor(height, width, bg, fontSize, textAlign) {
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;
+      this.textAlign = textAlign;
+    }
+
+    createDiv() {
+      let elem = document.createElement("div");
+      document.body.appendChild(elem);
+      let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+      elem.style.cssText = param;
+    }
+  }
+
+  const item = new Options(300, 350, "red", 14, "center");
+
+  // item.createDiv();
 });
